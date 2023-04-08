f0c53e830c6a283420b10cf3b319672c5814d199
Your Name <you@example.com>
2023-04-08 10:04:51 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const text = document.getElementById("text");
+
+const color = prompt("Rang yozing");
+const size = prompt("size yozing");
+
+text.style.color = color;
+text.style.fontSize = size;
+
+
+const text__p = document.getElementById("text__p");
+
+const Color = prompt(" p Rang yozing");
+const Size = prompt("  p size yozing");
+
+text__p.style.color = color;
+text__p.style.fontSize = size;
